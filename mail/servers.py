51123eeb6905e2ff5ee19536dcfb0a98e5f60898
Charles.Luo <charlesl@kainos.com>
2019-12-24 12:44:06 +0000

    new module 'servers' for managing SMTP and POP3 connections

---
+++ mail/servers.py
@@ -0,0 +1,34 @@
+import poplib
+import smtplib
+
+MAIL_SERVER='localhost'
+MAIL_SERVER_USER = "test18"
+MAIL_SERVER_PASS = "password"
+SMTP_PORT= 587
+POP3_PORT= 995
+
+class MailServer(object):
+
+    def __init__(self):
+        pass
+
+    def send_email(self, sender_email, receiver_email, message):
+        server = smtplib.SMTP(MAIL_SERVER, str(SMTP_PORT))
+        server.starttls()
+        server.login(MAIL_SERVER_USER, MAIL_SERVER_PASS)
+        server.sendmail(sender_email, receiver_email, message)
+        server.quit()
+
+    def read_email(self):
+        server = poplib.POP3_SSL(MAIL_SERVER, str(POP3_PORT))
+        server.user(MAIL_SERVER_USER)
+        server.pass_(MAIL_SERVER_PASS)
+        msg = self._process_contents(server)
+        server.quit()
+        return msg
+
+    def _process_contents(self, server):
+        msg_obj = server.list()
+        output=str(msg_obj) + "\n" + "\n"
+        output+=str(server.retr(len(msg_obj[1])))
+        return output
--- mail/views.py
+++ mail/views.py
@@ -1,34 +1,33 @@
-import poplib
-import smtplib
-import schedule
-import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
-from email import encoders
 from django.http import JsonResponse
 from rest_framework.status import HTTP_200_OK
 from rest_framework.views import APIView
+from mail.servers import MailServer
 
 class SendMailView(APIView):
     def get(self, request):
-        sender_email = "charles@example.com"
-        receiver_email = "test18@example.com"
+        server = MailServer()
+        server.send_email('charles@example.com','test18@example.com',
+                          self._build_message('charles@example.com', 'test18@example.com'))
+        return JsonResponse(status=HTTP_200_OK, data={"message": "email_sent !"})
+
+    def _build_message(self, sender, receiver):
         msg = MIMEMultipart()
         # storing the senders email address
-
-        msg['From'] = sender_email
+        msg['From'] = sender
         # storing the receivers email address
-        msg['To'] = receiver_email
+        msg['To'] = receiver
         # storing the subject
-        msg['Subject'] = "Subject of the Mail"
+        msg['Subject'] = "Subject of the Mail run number: 99"
         # string to store the body of the mail
         body = "Body_of_the_mail"
         # attach the body with the msg instance
         msg.attach(MIMEText(body, 'plain'))
         # open the file to be sent
         filename = "File_name_with_extension"
-        attachment = open("path_to_file", "rb")
+        attachment = open('<path_to_file>', 'rb')
         # instance of MIMEBase and named as p
         p = MIMEBase('application', 'octet-stream')
         # To change the payload into encoded form
@@ -38,31 +37,20 @@ class SendMailView(APIView):
         p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
         # attach the instance 'p' to instance 'msg'
         msg.attach(p)
-        server = smtplib.SMTP("localhost", "587")
-        server.starttls()
-        server.login("test18", "password")
-        server.sendmail(sender_email, receiver_email, msg.as_string())
-        server.quit()
-        return JsonResponse(status=HTTP_200_OK, data={"message": "email_sent?"})
+        return msg.as_string()
 
 class ReceiveMailView(APIView):
     def get(self, request):
-        server = poplib.POP3_SSL("localhost", 995)
-        server.user("test18")
-        server.pass_("password")
-        messages_info = server.list()
-
-        output=str(messages_info) + "\n" + "\n"
-        output+=str(server.retr(len(messages_info[1])))
-        print(output)
-        server.quit()
-        return JsonResponse(status=HTTP_200_OK, data=output, safe=False)
+       server = MailServer()
+       last_msg = server.read_email()
+       return JsonResponse(status=HTTP_200_OK, data=last_msg, safe=False)
 
 
 def job():
-   print(time.time())
+    pass
+   # print(time.time())
 
-schedule.every(5).seconds.do(job)
+# schedule.every(5).seconds.do(job)
 # schedule.every().hour.do(job)
 # schedule.every().day.at("10:30").do(job)
 # schedule.every().monday.do(job)
