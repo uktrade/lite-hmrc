ad5d1082c17c1e106cc57503763e51f00dde2c3b
cstobart <c.stobart@kainos.com>
2020-01-09 16:40:05 +0000

    Update data_processing.py

--- mail/data_processing.py
+++ mail/data_processing.py
@@ -7,7 +7,9 @@ from mail.serializers import LicenseUpdateSerializer, InvalidEmailSerializer
 def process_and_save_email_message(dto: EmailMessageDto):
     data = {}
     last_hmrc_number = LicenseUpdate.objects.last().hmrc_run_number
-    data["hmrc_run_number"] = last_hmrc_number + 1 if last_hmrc_number != 99999 else 0
+    data["hmrc_run_number"] = (
+        last_hmrc_number + 1 if last_hmrc_number != 99999 else 0
+    )  # TODO: Extra logic to generalise
     data["source_run_number"] = dto.run_number
     data["edi_filename"], data["edi_data"] = process_attachment(dto.attachment)
     data["extract_type"] = "insert"  # TODO: extract from data
