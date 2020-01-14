9a8baf62f1dd00bcf5c7502b8b741ca0a9f52c1a
cstobart <c.stobart@kainos.com>
2020-01-14 15:40:24 +0000

    nest serializers

--- mail/serializers.py
+++ mail/serializers.py
@@ -1,9 +1,18 @@
+from django.db import transaction
 from rest_framework import serializers
 
-from mail.models import Mail
+from mail.models import Mail, LicenceUpdate
 
 
 class LicenceUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LicenceUpdate
+        fields = "__all__"
+
+
+class LicenceUpdateMailSerializer(serializers.ModelSerializer):
+    licence_update = LicenceUpdateSerializer(write_only=True)
+
     class Meta:
         model = Mail
         fields = [
@@ -13,7 +22,23 @@ class LicenceUpdateSerializer(serializers.ModelSerializer):
             "edi_data",
             "extract_type",
             "raw_data",
+            "licence_update",
         ]
+
+    @transaction.atomic
+    def create(self, validated_data):
+        print(validated_data)
+        licence_update_data = validated_data.pop("licence_update")
+        mail = Mail.objects.create(**validated_data)
+        licence_update_data["mail"] = mail.id
+
+        licence_update_serializer = LicenceUpdateSerializer(data=licence_update_data)
+        if licence_update_serializer.is_valid():
+            licence_update_serializer.save()
+        else:
+            raise serializers.ValidationError(licence_update_serializer.errors)
+
+        return mail
 
 
 class LicenceUpdateUpdateSerializer(serializers.ModelSerializer):
--- mail/services/data_processing.py
+++ mail/services/data_processing.py
@@ -1,7 +1,7 @@
 from mail.dtos import EmailMessageDto
 from mail.enums import SourceEnum
 from mail.models import LicenceUpdate
-from mail.serializers import InvalidEmailSerializer, LicenceUpdateSerializer
+from mail.serializers import InvalidEmailSerializer, LicenceUpdateMailSerializer
 from mail.services.helpers import (
     convert_sender_to_source,
     process_attachment,
@@ -10,8 +10,11 @@ from mail.services.helpers import (
 
 def process_and_save_email_message(dto: EmailMessageDto):
     data = convert_dto_data_for_serialization(dto)
+    print("\n")
+    print(data)
+    print("\n")
 
-    serializer = LicenceUpdateSerializer(data=data)
+    serializer = LicenceUpdateMailSerializer(data=data)
 
     if serializer.is_valid():
         serializer.save()
@@ -25,24 +28,34 @@ def process_and_save_email_message(dto: EmailMessageDto):
 
 
 def convert_dto_data_for_serialization(dto: EmailMessageDto):
-    data = {}
-    data["source"] = convert_sender_to_source(dto.sender)
+    data = {"licence_update": {}}
+    data["licence_update"]["source"] = convert_sender_to_source(dto.sender)
+
+    # Needs working out
     if convert_sender_to_source(dto.sender) == SourceEnum.SPIRE:
         last_licence_update = LicenceUpdate.objects.last()
         if not last_licence_update.source_run_number == dto.run_number:
-            data["hmrc_run_number"] = (
-                last_licence_update.hmrc_run_number + 1
-                if last_licence_update.hmrc_run_number != 99999
-                else 0
-            )
+            if last_licence_update:
+                data["licence_update"]["hmrc_run_number"] = (
+                    last_licence_update.hmrc_run_number + 1
+                    if last_licence_update.hmrc_run_number != 99999
+                    else 0
+                )
+            else:
+                data["licence_update"]["hmrc_run_number"] = 1
         else:
-            data["hmrc_run_number"] = last_licence_update.hmrc_run_number
-    data["source_run_number"] = dto.run_number
+            data["licence_update"][
+                "hmrc_run_number"
+            ] = last_licence_update.hmrc_run_number
+    elif convert_sender_to_source(dto.sender) == SourceEnum.LITE:
+        data["licence_update"]["hmrc_run_number"] = 1
+
+    data["licence_update"]["source_run_number"] = dto.run_number
 
     data["edi_filename"], data["edi_data"] = process_attachment(dto.attachment)
 
     data["extract_type"] = "insert"  # TODO: extract from data
-    data["license_ids"] = [
+    data["licence_update"]["license_ids"] = [
         "00000000-0000-0000-0000-000000000001"
     ]  # TODO: extract from data
     data["raw_data"] = dto.raw_data
