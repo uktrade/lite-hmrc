f6b3c3f8cac19479134dbb26397d740458dbf839
cstobart <c.stobart@kainos.com>
2020-02-20 09:18:59 +0000

    Update scheduler.py

--- mail/scheduling/scheduler.py
+++ mail/scheduling/scheduler.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 import threading
 import time
 
@@ -6,29 +7,19 @@ from conf.settings import POLL_INTERVAL
 
 
 def scheduled_job():
-    # Do some stuff
-    # Offload the blocking job to a new thread
-
-    t = threading.Thread(target=some_fn, args=(True,))
+    t = threading.Thread(target=polling_job, args=(True,))
     t.setDaemon(True)
     t.start()
 
     return True
 
 
-def some_fn(x):
+def polling_job(x):
     while x:
         from mail.routing_controller import check_and_route_emails
 
-        print("I'm tyring to connect", datetime.datetime.now())
         try:
             check_and_route_emails()
         except Exception as e:
-            print(e)
-        print("I'm all good", datetime.datetime.now())
-        # if condition:
-        #     return
-        # else:
-        #     time.sleep(interval in seconds)
-        # return 0
+            logging.info({"message": "liteolog hmrc", "status": "Exception: " + str(e)})
         time.sleep(POLL_INTERVAL)
