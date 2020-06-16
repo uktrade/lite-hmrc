735aa06a904cf23ae9fc679e51d4392c3e2945f8
Andy Taylor <a.taylor@kainos.com>
2020-06-16 10:54:47 +0100

    email for rejections includes id and timestamp

--- mail/models.py
+++ mail/models.py
@@ -1,6 +1,7 @@
 import json
 import uuid
 from datetime import timedelta
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from typing import List
 
@@ -8,7 +9,10 @@ from django.db import models
 from django.db.models import Q
 from django.utils import timezone
 from jsonfield import JSONField
+
+from conf.settings import EMAIL_USER, NOTIFY_USERS
 from mail.enums import ReceptionStatusEnum, ExtractTypeEnum, SourceEnum, LicenceActionEnum, ReplyStatusEnum
+from mail.servers import MailServer
 
 
 class MailManager(models.Manager):
@@ -54,7 +58,7 @@ class Mail(models.Model):
         super(Mail, self).save(*args, **kwargs)
 
         if self.response_data and ReplyStatusEnum.REJECTED in self.response_data:
-            self.send_rejection_notification_email()
+            self.send_rejection_notification_email(self.id, self.response_date)
 
     def set_locking_time(self, offset: int = 0):
         self.currently_processing_at = timezone.now() + timedelta(seconds=offset)
@@ -69,17 +73,14 @@ class Mail(models.Model):
         self.save()
 
     @staticmethod
-    def send_rejection_notification_email():
-        from conf.settings import EMAIL_USER, NOTIFY_USERS
-        from email.mime.multipart import MIMEMultipart
+    def send_rejection_notification_email(id, response_date):
         from mail.libraries.mailbox_service import send_email
-        from mail.servers import MailServer
 
         multipart_msg = MIMEMultipart()
         multipart_msg["From"] = EMAIL_USER
         multipart_msg["To"] = ",".join(NOTIFY_USERS)
-        multipart_msg["Subject"] = "FAIIIIL lol"
-        body = MIMEText("FAIIIIIL")
+        multipart_msg["Subject"] = f"Mail rejected"
+        body = MIMEText(f"Mail [{id}] received at [{response_date}] was rejected")
         multipart_msg.attach(body)
 
         server = MailServer()
