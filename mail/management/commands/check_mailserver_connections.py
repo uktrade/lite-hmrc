0cc681fa11d5518bba57da4dd947e519b0f8177e
Kevin Carrogan <kevin@carrogan.com>
2023-06-25 16:12:14 +0100

    Update management command to check pop3 connection
    Will now log connection errors and move to checking the next mailbox instead of terminating completely on a failure

--- mail/management/commands/check_mailserver_connections.py
+++ mail/management/commands/check_mailserver_connections.py
@@ -1,3 +1,5 @@
+import poplib
+
 from django.core.management import BaseCommand
 
 from mail.libraries.routing_controller import get_hmrc_to_dit_mailserver, get_spire_to_dit_mailserver
@@ -14,6 +16,12 @@ class Command(BaseCommand):
         for mailserver_to_check_factory in mailservers_to_check_factories:
             self.stdout.write(f"Checking {mailserver_to_check_factory.__name__}")
             mailserver = mailserver_to_check_factory()
-            pop3_connection = mailserver.connect_to_pop3()
-            self.stdout.write(pop3_connection.welcome.decode("ascii"))
-            mailserver.quit_pop3_connection()
+            try:
+                pop3_connection = mailserver.connect_to_pop3()
+            except poplib.error_proto as e:
+                response, *_ = e.args
+                self.stdout.write(self.style.ERROR(response))
+            else:
+                self.stdout.write(pop3_connection.welcome.decode("ascii"))
+            finally:
+                mailserver.quit_pop3_connection()
