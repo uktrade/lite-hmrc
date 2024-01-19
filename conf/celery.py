fe04c95488059c291df3b9655fd201c693097e5b
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-01-19 15:13:58 +0000

    Configure beat schedule for the task that sends licence details to HMRC
    Run this task every 10 min to see if any licence details available that
    needs sending to HMRC.

--- conf/celery.py
+++ conf/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
@@ -16,3 +17,11 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 # Load celery_tasks.py modules from all registered Django apps.
 app.autodiscover_tasks(related_name="celery_tasks")
 app.autodiscover_tasks(["core"], related_name="celery_tasks")
+
+# Define any regular scheduled tasks
+app.conf.beat_schedule = {
+    "send licence details to hmrc, periodic task every 10min": {
+        "task": "mail.celery_tasks.send_licence_details_to_hmrc",
+        "schedule": crontab(minute="*/10"),
+    },
+}
