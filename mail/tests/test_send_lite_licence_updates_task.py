d6c91fd90756c8fe8f3f8f4466c91cd35a71e22a
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-01-22 17:28:51 +0000

    Refactor tests that test sending licence details task
    The existing tests are not clear what they are testing and doesn't
    look correct. The main thing we need to check is processing of
    payload when there are active mails or not which was not happening
    in existing tests.

--- mail/tests/test_send_lite_licence_updates_task.py
+++ mail/tests/test_send_lite_licence_updates_task.py
@@ -1,6 +1,7 @@
 from unittest import mock
 
 from django.test import override_settings
+from parameterized import parameterized
 
 from mail.celery_tasks import send_licence_details_to_hmrc
 from mail.enums import ReceptionStatusEnum
@@ -12,53 +13,53 @@ from mail.tests.libraries.client import LiteHMRCTestClient
 @override_settings(BACKGROUND_TASK_ENABLED=False)  # Disable task from being run on app initialization
 class TaskTests(LiteHMRCTestClient):
     def setUp(self):
-        self.mail = Mail.objects.create(edi_filename="filename", edi_data="1\\fileHeader\\CHIEF\\SPIRE\\")
         super().setUp()
+        self.mail = Mail.objects.create(edi_filename="filename", edi_data="1\\fileHeader\\CHIEF\\SPIRE\\")
 
+    @parameterized.expand(
+        [
+            (ReceptionStatusEnum.PENDING, 1, 1, 0),
+            (ReceptionStatusEnum.REPLY_PENDING, 1, 1, 0),
+            (ReceptionStatusEnum.REPLY_RECEIVED, 1, 1, 0),
+            (ReceptionStatusEnum.REPLY_SENT, 2, 1, 1),
+        ]
+    )
     @mock.patch("mail.celery_tasks.send")
-    def test_pending(self, send):
-        self.mailstatus = ReceptionStatusEnum.PENDING
+    def test_send_licence_details_with_active_mail_status(
+        self, mail_status, expected_mail_count, payload_count, processed_payload_count, mock_send
+    ):
+        """
+        We can only send one message at a time to HMRC so before we send next message the
+        previous message should be fully processed (status will be 'reply_sent') so before sending
+        details we need to check if there is an active email.
+        In this test we ensure payload is only processed where there are no active emails.
+        """
+        self.mail.status = mail_status
         self.mail.save()
-        send.return_value = None
+
+        self.assertEqual(LicencePayload.objects.count(), payload_count)
+
         send_licence_details_to_hmrc.delay()
-        self.assertEqual(LicencePayload.objects.filter(is_processed=True).count(), 0)
+        self.assertEqual(Mail.objects.count(), expected_mail_count)
+        self.assertEqual(LicencePayload.objects.filter(is_processed=True).count(), processed_payload_count)
 
     @mock.patch("mail.celery_tasks.send")
-    def test_reply_pending(self, send):
-        self.mail.status = ReceptionStatusEnum.REPLY_PENDING
-        self.mail.save()
-        send.return_value = None
-        send_licence_details_to_hmrc.delay()
-        self.assertEqual(LicencePayload.objects.filter(is_processed=True).count(), 0)
-
-    @mock.patch("mail.celery_tasks.send")
-    def test_reply_received(self, send):
-        self.mail.status = ReceptionStatusEnum.REPLY_RECEIVED
-        self.mail.save()
-        send.return_value = None
-        send_licence_details_to_hmrc.delay()
-        self.assertEqual(LicencePayload.objects.filter(is_processed=True).count(), 0)
-
-    @mock.patch("mail.celery_tasks.send")
-    def test_reply_sent_rejected(self, send):
-        self.mail.status = "reply_sent"
-        self.mail.response_data = "rejected"
-        self.mail.save()
-        send.return_value = None
-        send_licence_details_to_hmrc.delay()
-        self.assertEqual(LicencePayload.objects.filter(is_processed=True).count(), 1)
-
-    @mock.patch("mail.celery_tasks.send")
-    def test_reply_sent_accepted(self, send):
+    def test_send_licence_details_not_sent_when_there_are_no_payloads(self, mock_send):
         self.mail.status = ReceptionStatusEnum.REPLY_SENT
-        self.mail.response_data = "accepted"
         self.mail.save()
-        send.return_value = None
+
+        # Mark existing payload as processed
+        self.single_siel_licence_payload.is_processed = True
+        self.single_siel_licence_payload.save()
+
+        self.assertEqual(LicencePayload.objects.count(), 1)
+
         send_licence_details_to_hmrc.delay()
-        self.assertEqual(LicencePayload.objects.filter(is_processed=True).count(), 1)
+        self.assertEqual(Mail.objects.count(), 1)
+        mock_send.assert_not_called()
 
     @mock.patch("mail.libraries.lite_to_edifact_converter.validate_edifact_file")
-    def test_exception(self, validator):
+    def test_send_licence_details_raises_exception(self, validator):
         validator.side_effect = EdifactValidationError()
         with self.assertRaises(EdifactValidationError):
             self.mail.status = ReceptionStatusEnum.REPLY_SENT
