6bcdee02ae72de8b0b9b21e4e60c5e81728e9b0f
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-01-24 17:11:40 +0000

    Update healthcheck task responsiveness tests to use celery fixtures

--- mail/tests/conftest.py
+++ mail/tests/conftest.py
@@ -2,6 +2,9 @@ import os
 import pytest
 
 from celery import Celery
+from celery.schedules import crontab
+
+from mail.celery_tasks import CELERY_SEND_LICENCE_UPDATES_TASK_NAME
 
 
 @pytest.fixture(autouse=True)
@@ -9,7 +12,21 @@ def celery_app():
     # Setup the celery worker to run in process for tests
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
     celeryapp = Celery("lite-hmrc")
+    celeryapp.config_from_object("django.conf:settings", namespace="CELERY_TEST")
+
     celeryapp.autodiscover_tasks(related_name="celery_tasks")
-    celeryapp.conf.update(CELERY_ALWAYS_EAGER=True)
-    celeryapp.conf.update(CELERY_TASK_STORE_EAGER_RESULT=True)
+    celeryapp.conf["CELERY_ALWAYS_EAGER"] = True
+    celeryapp.conf["CELERY_TASK_STORE_EAGER_RESULT"] = True
+
+    celeryapp.conf["beat_schedule"] = {
+        # send licence details to hmrc, periodic task every 10min
+        CELERY_SEND_LICENCE_UPDATES_TASK_NAME: {
+            "task": CELERY_SEND_LICENCE_UPDATES_TASK_NAME,
+            "schedule": crontab(minute="*/10"),
+        },
+    }
+
+    # Make this the default app for all threads
+    celeryapp.set_default()
+
     return celeryapp
