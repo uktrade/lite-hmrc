b1683ea3c2e9f4e681da5e6f3ab396b58e851ab3
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-06-28 13:52:00 +0100

    Update test_end_to_end.py

--- mail/tests/test_end_to_end.py
+++ mail/tests/test_end_to_end.py
@@ -46,8 +46,12 @@ class EndToEndTests(LiteHMRCTestClient):
 6\restrictions\Provisos may apply please see licence
 7\line\1\\\\\Sporting shotgun\Q\\030\\10\\\\\\
 8\line\2\\\\\Stock\Q\\111\\11.0\\\\\\
-9\end\licence\8
-10\fileTrailer\1"""
+9\n11\\line\\3\\\\\\\\\\Metal\\Q\\\\025\\\\1.0\\\\\\\\\\\\
+10\n12\\line\\4\\\\\\\\\\Chemical\\Q\\\\116\\\\20.0\\\\\\\\\\\\
+11\n13\\line\\5\\\\\\\\\\Chemical\\Q\\\\110\\\\20.0\\\\\\\\\\\\
+12\n14\\line\\6\\\\\\\\\\Chemical\\Q\\\\074\\\\20.0\\\\\\\\\\\\
+139\end\licence\12
+14\fileTrailer\1"""
         assert body == expected_mail_body  # nosec
         encoded_reference_code = quote("GBSIEL/2020/0000001/P", safe="")
         response = self.client.get(f"{reverse('mail:licence')}?id={encoded_reference_code}")
