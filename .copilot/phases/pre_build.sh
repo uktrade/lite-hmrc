2dacfcaf16b1efe242c19fdb2b2b67b6bcf18c7c
James Francis <james.francis@madetech.com>
2024-07-03 11:48:23 +0100

    add prebuild script to get submodules

--- .copilot/phases/pre_build.sh
+++ .copilot/phases/pre_build.sh
@@ -3,4 +3,17 @@
 # Exit early if something goes wrong
 set -e
 
-# Add commands below to run as part of the pre_build phase
+git_clone_base_url="https://codestar-connections.eu-west-2.amazonaws.com/git-http/730335529260/eu-west-2/192881c6-e3f2-41a9-9dcb-fcc87d8b90be/uktrade"
+
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
+
+cat <<EOF > ./.gitmodules
+[submodule "django_db_anonymiser"]
+	path = django_db_anonymiser
+	url = $git_clone_base_url/django-db-anonymiser.git
+EOF
+
+git submodule update --init --remote --recursive
+
+echo "done"
