72486fcdb5e99d9a2d153ee0e9cb84b171d78574
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-07-10 11:43:01 +0100

    Add debugging for failed build

--- .copilot/phases/post_build.sh
+++ .copilot/phases/post_build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 # Exit early if something goes wrong
-set -e
+set -ex
 
 # Add commands below to run as part of the post_build phase
+
+ls -al /layers/paketo-buildpacks_pipenv-install/packages/workspace-dqq3IVyd/
+
+which python
+
+python --version
+
+pipenv install --deploy --verbose
