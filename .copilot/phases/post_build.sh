26be92163e4a6f6e11a182a1c86fc4e29a88b884
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-07-10 11:58:29 +0100

    Debug pack

--- .copilot/phases/post_build.sh
+++ .copilot/phases/post_build.sh
@@ -5,12 +5,4 @@ set -x
 
 # Add commands below to run as part of the post_build phase
 
-ls -al /layers
-
-ls -al /layers/paketo-buildpacks_pipenv-install/packages/
-
-which python
-
-python --version
-
-pipenv install --deploy --verbose
+which pack
