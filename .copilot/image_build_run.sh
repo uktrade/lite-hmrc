7acc9f1551b0e85ac3bd0b07fca108a951225af6
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-07-10 12:00:50 +0100

    Debug in image_build_run.sh

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # Exit early if something goes wrong
-set -e
+set -x
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
+
+ls -al /layers/paketo-buildpacks_pipenv-install/packages/workspace-dqq3IVyd/
