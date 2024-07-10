5585ede65cfe1e1da48e1f17ded7e97da1f745b5
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-07-10 14:21:38 +0100

    Remove debugging

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -4,5 +4,3 @@
 set -x
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
-
-ls -al /layers/paketo-buildpacks_pipenv-install/packages/workspace-dqq3IVyd/
