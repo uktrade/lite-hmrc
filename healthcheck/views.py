d755b333d0b9bdff6df271698c1f2af70741bc86
James Francis <james.francis@madetech.com>
2024-07-12 14:02:38 +0100

    add additional debug lines

--- healthcheck/views.py
+++ healthcheck/views.py
@@ -6,6 +6,9 @@ class HealthCheckPingdomView(MainView):
 
     def render_to_response(self, context, status):
         context["errored_plugins"] = [plugin for plugin in context["plugins"] if plugin.errors]
-        print(context["errored_plugins"])
+        print("********* errored_plugins: ", context["errored_plugins"])
+        [print(plugin) for plugin in context["plugins"]]
+        print("********* context: ", context)
+        print("********* status: ", status)
         context["total_response_time"] = sum([plugin.time_taken for plugin in context["plugins"]])
         return super().render_to_response(context=context, status=status, content_type="text/xml")
