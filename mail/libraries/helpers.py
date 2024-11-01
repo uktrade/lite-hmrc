2bdcc826bdc60e6f04503ce2a789a93dda97f4a2
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2024-11-01 14:24:56 +0000

    Make sentry scoping compatible with sentry-sdk 1.X

--- mail/libraries/helpers.py
+++ mail/libraries/helpers.py
@@ -338,7 +338,7 @@ def sort_dtos_by_date(input_dtos):
 
 def log_to_sentry(message, extra=None, level="info"):
     extra = extra or {}
-    with sentry_sdk.new_scope() as scope:
+    with sentry_sdk.push_scope() as scope:
         for key, value in extra.items():
             scope.set_extra(key, value)
         sentry_sdk.capture_message(message, level=level)
