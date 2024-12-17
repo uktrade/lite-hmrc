d5352334e041dd9aef51abe1d965681a8bc36769
Kevin Carrogan <kevin@carrogan.com>
2024-12-17 12:56:50 +0000

    Only add APM app if URL is set locally

--- conf/settings.py
+++ conf/settings.py
@@ -358,7 +358,6 @@ else:
     # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
     DATABASES = {"default": env.db()}
     REDIS_BASE_URL = env("REDIS_BASE_URL", default=None)
-    INSTALLED_APPS.append("elasticapm.contrib.django")
     DB_ANONYMISER_AWS_ENDPOINT_URL = AWS_ENDPOINT_URL
     DB_ANONYMISER_AWS_ACCESS_KEY_ID = env("DB_ANONYMISER_AWS_ACCESS_KEY_ID", default=None)
     DB_ANONYMISER_AWS_SECRET_ACCESS_KEY = env("DB_ANONYMISER_AWS_SECRET_ACCESS_KEY", default=None)
