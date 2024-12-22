4228a40665c683bfbd21a61300cce594547b7e7e
Kevin Carrogan <kevin@carrogan.com>
2024-12-22 22:56:12 +0000

    Change API for mailbox functions to separate out responsibilities

--- mailboxes/tests/test_utils.py
+++ mailboxes/tests/test_utils.py
@@ -3,8 +3,9 @@ from email.message import Message
 from unittest.mock import MagicMock
 
 from django.test import SimpleTestCase, override_settings
+from parameterized import parameterized
 
-from mailboxes.utils import get_message_header, get_message_id
+from mailboxes.utils import get_message_header, get_message_id, is_from_valid_sender
 
 
 class GetMessageHeaderTests(SimpleTestCase):
@@ -16,11 +17,10 @@ class GetMessageHeaderTests(SimpleTestCase):
         pop3_connection = MagicMock()
         pop3_connection().top.return_value = MagicMock(), msg.as_bytes().split(b"\n"), MagicMock()
 
-        returned_header, msg_num = get_message_header(pop3_connection(), "4 12345")
+        returned_header = get_message_header(pop3_connection(), "4")
         pop3_connection().top.assert_called_with("4", 0)
         self.assertIsInstance(returned_header, Message)
         self.assertEqual(returned_header["header"], "value")
-        self.assertEqual(msg_num, "4")
 
 
 class GetMessageId(SimpleTestCase):
@@ -30,30 +30,41 @@ class GetMessageId(SimpleTestCase):
     )
     def test_get_message_id_not_from_valid_email(self):
         message = Message()
-        from_header = Header("from@example.com")  # /PS-IGNORE
-        message["From"] = from_header
-        reply_to_header = Header("reply-to@example.com")  # /PS-IGNORE
-        message["Reply-To"] = reply_to_header
-
-        message_id, message_number = get_message_id(message, "4")
-
-        self.assertIsNone(message_id)
-        self.assertEqual(message_number, "4")
-
-    @override_settings(
-        SPIRE_FROM_ADDRESS="spire.from.address@example.com",  # /PS-IGNORE
-        HMRC_TO_DIT_REPLY_ADDRESS="hmrc.to.dit.reply.address@example.com",  # /PS-IGNORE
-    )
-    def test_get_message_id_not_from_valid_email(self):
-        message = Message()
-        from_header = Header("Spire <spire.from.address@example.com>")  # /PS-IGNORE
-        message["From"] = from_header
-        reply_to_header = Header("HMRC <hmrc.to.dit.reply.address@example.com>")  # /PS-IGNORE
-        message["Reply-To"] = reply_to_header
         message_id_header = Header("<123456@example.com>")  # /PS-IGNORE
         message["Message-ID"] = message_id_header
 
-        message_id, message_number = get_message_id(message, "4")
+        message_id = get_message_id(message)
 
         self.assertEqual(message_id, "123456")
-        self.assertEqual(message_number, "4")
+
+
+class IsFromValidSenderTests(SimpleTestCase):
+    @parameterized.expand(
+        [
+            "From: spire.from.address@example.com",  # /PS-IGNORE
+            "From: hmrc.to.dit.reply.address@example.com",  # /PS-IGNORE
+            "spire.from.address@example.com",  # /PS-IGNORE
+            "hmrc.to.dit.reply.address@example.com",  # /PS-IGNORE
+        ]
+    )
+    def test_is_from_valid_sender(self, valid_address):
+        message = Message()
+        from_header = Header(f"Sender <{valid_address.replace('From: ', '')}>")  # /PS-IGNORE
+        message["From"] = from_header
+
+        self.assertTrue(is_from_valid_sender(message, [valid_address]))
+
+    @parameterized.expand(
+        [
+            "From: spire.from.address@example.com",  # /PS-IGNORE
+            "From: hmrc.to.dit.reply.address@example.com",  # /PS-IGNORE
+            "spire.from.address@example.com",  # /PS-IGNORE
+            "hmrc.to.dit.reply.address@example.com",  # /PS-IGNORE
+        ]
+    )
+    def test_is_from_invalid_sender(self, valid_address):
+        message = Message()
+        from_header = Header("Invalid <not.valid@example.com>")  # /PS-IGNORE
+        message["From"] = from_header
+
+        self.assertFalse(is_from_valid_sender(message, [valid_address]))
--- mailboxes/utils.py
+++ mailboxes/utils.py
@@ -3,14 +3,10 @@ from email.headerregistry import Address
 from email.parser import BytesHeaderParser
 from email.utils import parseaddr
 
-from django.conf import settings
-
 logger = logging.getLogger(__name__)
 
 
-def get_message_header(pop3_connection, listing_msg):
-    msg_num = listing_msg.split()[0]
-
+def get_message_header(pop3_connection, msg_num):
     # retrieves the header information
     # 0 indicates the number of lines of message to be retrieved after the header
     _, msg_header, _ = pop3_connection.top(msg_num, 0)
@@ -18,24 +14,17 @@ def get_message_header(pop3_connection, listing_msg):
     parser = BytesHeaderParser()
     header = parser.parsebytes(b"".join(msg_header))
 
-    return header, msg_num
+    return header
 
 
-def get_message_id(msg_header, msg_num):
-    spire_from_address = settings.SPIRE_FROM_ADDRESS.encode("utf-8")
-    hmrc_dit_reply_address = settings.HMRC_TO_DIT_REPLY_ADDRESS.encode("utf-8")
-
-    if spire_from_address not in msg_header.as_bytes() and hmrc_dit_reply_address not in msg_header.as_bytes():
-        logger.warning(
-            "Found mail with message_num %s that is not from SPIRE (%s) or HMRC (%s), skipping ...",
-            msg_num,
-            spire_from_address,
-            hmrc_dit_reply_address,
-        )
-        return None, msg_num
-
+def get_message_id(msg_header):
     _, message_id = parseaddr(str(msg_header["message-id"]))
     message_id = Address(addr_spec=message_id).username
+    return message_id
 
-    logging.info("Extracted Message-Id as %s for the message_num %s", message_id, msg_num)
-    return message_id, msg_num
+
+def is_from_valid_sender(msg_header, valid_addresses):
+    _, from_address = parseaddr(str(msg_header["From"]))
+    valid_addresses = [address.replace("From: ", "") for address in valid_addresses]
+
+    return from_address in valid_addresses
