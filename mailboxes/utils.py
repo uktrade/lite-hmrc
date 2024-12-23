5d694d05a2a48165820fa60cf1b5af717218dfd4
Kevin Carrogan <kevin@carrogan.com>
2024-12-23 14:29:06 +0000

    Split out getting unread messages into separate function

--- mailboxes/utils.py
+++ mailboxes/utils.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 from email.headerregistry import Address
 from email.parser import BytesHeaderParser
 from email.utils import parseaddr
@@ -42,6 +43,33 @@ def get_read_messages(mailbox_config):
     )
     read_message_ids = set(mail_read_statuses.values_list("message_id", flat=True))
     return read_message_ids
+
+
+def get_unread_message_ids(pop3_connection, mailbox_config, incoming_email_check_limit=sys.maxsize):
+    read_messages = get_read_messages(mailbox_config)
+    logger.info("Number of messages READ/UNPROCESSABLE in %s are %s", mailbox_config.username, len(read_messages))
+
+    _, mails, _ = pop3_connection.list()
+    mails = mails[-incoming_email_check_limit:]
+    for mail in mails:
+        message_num = get_message_number(mail)
+        message_header = get_message_header(pop3_connection, message_num)
+
+        if not is_from_valid_sender(message_header, [settings.SPIRE_FROM_ADDRESS, settings.HMRC_TO_DIT_REPLY_ADDRESS]):
+            logger.warning(
+                "Found mail with message_num %s that is not from SPIRE (%s) or HMRC (%s), skipping ...",
+                message_num,
+                settings.SPIRE_FROM_ADDRESS,
+                settings.HMRC_TO_DIT_REPLY_ADDRESS,
+            )
+            continue
+
+        message_id = get_message_id(message_header)
+        logger.info("Extracted Message-Id as %s for the message_num %s", message_id, message_num)
+        if message_id in read_messages:
+            continue
+
+        yield message_id, message_num
 
 
 def is_from_valid_sender(msg_header, valid_addresses):
