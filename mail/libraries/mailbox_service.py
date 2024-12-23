2645beb39f86b56774a2be5f4739625dfa62e275
Kevin Carrogan <kevin@carrogan.com>
2024-12-23 18:44:34 +0000

    Move out message iterator function

--- mail/libraries/mailbox_service.py
+++ mail/libraries/mailbox_service.py
@@ -1,83 +1,12 @@
 import logging
-from poplib import POP3_SSL, error_proto
-from typing import Callable, Iterator, List, Tuple
+from typing import List, Optional
 
-from django.conf import settings
-
-from mail.libraries.email_message_dto import EmailMessageDto
-from mail.libraries.helpers import to_mail_message_dto
 from mail.models import Mail
-from mailboxes.enums import MailReadStatuses
-from mailboxes.models import MailboxConfig
-from mailboxes.utils import get_unread_message_ids
 
 logger = logging.getLogger(__name__)
 
 
-def get_message_iterator(pop3_connection: POP3_SSL, username: str) -> Iterator[Tuple[EmailMessageDto, Callable]]:
-    mailbox_config, _ = MailboxConfig.objects.get_or_create(username=username)
-
-    for message_id, message_num in get_unread_message_ids(
-        pop3_connection,
-        mailbox_config,
-        settings.INCOMING_EMAIL_CHECK_LIMIT,
-    ):
-        read_status, _ = mailbox_config.mail_read_statuses.get_or_create(
-            message_id=message_id,
-            message_num=message_num,
-        )
-
-        def mark_status(status):
-            """
-            :param status: A choice from `MailReadStatuses.choices`
-            """
-            logger.info(
-                "Marking message_id %s with message_num %s from %s as %s",
-                message_id,
-                message_num,
-                read_status.mailbox,
-                status,
-            )
-            read_status.status = status
-            read_status.save()
-
-        try:
-            m = pop3_connection.retr(message_num)
-            logger.info(
-                "Retrieved message_id %s with message_num %s from %s",
-                message_id,
-                message_num,
-                read_status.mailbox,
-            )
-        except error_proto as err:
-            logger.error(
-                "Unable to RETR message num %s with Message-ID %s in %s: %s",
-                message_num,
-                message_id,
-                mailbox_config,
-                err,
-                exc_info=True,
-            )
-            continue
-
-        try:
-            mail_message = to_mail_message_dto(m)
-        except ValueError as ve:
-            logger.error(
-                "Unable to convert message num %s with Message-Id %s to DTO in %s: %s",
-                message_num,
-                message_id,
-                mailbox_config,
-                ve,
-                exc_info=True,
-            )
-            mark_status(MailReadStatuses.UNPROCESSABLE)
-            continue
-
-        yield mail_message, mark_status
-
-
-def find_mail_of(extract_types: List[str], reception_status: str) -> Mail or None:
+def find_mail_of(extract_types: List[str], reception_status: str) -> Optional[Mail]:
     try:
         mail = Mail.objects.get(status=reception_status, extract_type__in=extract_types)
     except Mail.DoesNotExist:
--- mailboxes/utils.py
+++ mailboxes/utils.py
@@ -3,10 +3,16 @@ import sys
 from email.headerregistry import Address
 from email.parser import BytesHeaderParser
 from email.utils import parseaddr
+from poplib import error_proto
+from typing import Callable, Iterator, Tuple
 
 from django.conf import settings
 
+from mail.libraries.email_message_dto import EmailMessageDto
+from mail.libraries.helpers import to_mail_message_dto
+from mail_servers.servers import MailServer
 from mailboxes.enums import MailReadStatuses
+from mailboxes.models import MailboxConfig
 
 logger = logging.getLogger(__name__)
 
@@ -77,3 +83,69 @@ def is_from_valid_sender(msg_header, valid_addresses):
     valid_addresses = [address.replace("From: ", "") for address in valid_addresses]
 
     return from_address in valid_addresses
+
+
+class MarkStatus:
+    def __init__(self, message_id, message_num, read_status):
+        self.message_id = message_id
+        self.message_num = message_num
+        self.read_status = read_status
+
+    def __call__(self, status):
+        logger.info(
+            "Marking message_id %s with message_num %s from %r as %s",
+            self.message_id,
+            self.message_num,
+            self.read_status.mailbox,
+            status,
+        )
+        self.read_status.status = status
+        self.read_status.save()
+
+
+def get_message_iterator(server: MailServer) -> Iterator[Tuple[EmailMessageDto, Callable]]:
+    mailbox_config, _ = MailboxConfig.objects.get_or_create(username=server.username)
+
+    with server.connect_to_pop3() as pop3_connection:
+        for message_id, message_num in get_unread_message_ids(
+            pop3_connection,
+            mailbox_config,
+            settings.INCOMING_EMAIL_CHECK_LIMIT,
+        ):
+            read_status, _ = mailbox_config.mail_read_statuses.get_or_create(
+                message_id=message_id,
+                message_num=message_num,
+            )
+
+            mark_status = MarkStatus(message_id, message_num, read_status)
+
+            try:
+                m = pop3_connection.retr(message_num)
+                logger.info(
+                    "Retrieved message_id %s with message_num %s from %s",
+                    message_id,
+                    message_num,
+                    read_status.mailbox,
+                )
+            except error_proto:
+                logger.exception(
+                    "Unable to RETR message num %s with Message-ID %s in %r",
+                    message_num,
+                    message_id,
+                    mailbox_config,
+                )
+                continue
+
+            try:
+                mail_message = to_mail_message_dto(m)
+            except ValueError:
+                mark_status(MailReadStatuses.UNPROCESSABLE)
+                logger.exception(
+                    "Unable to convert message num %s with Message-Id %s to DTO in %r",
+                    message_num,
+                    message_id,
+                    mailbox_config,
+                )
+                continue
+
+            yield mail_message, mark_status
