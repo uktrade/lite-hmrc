f192f0d24c427a51de951bb5e1f9d1e618a78abc
Kevin Carrogan <kevin@carrogan.com>
2024-12-29 19:44:13 +0000

    Update assertions to check more expected interactions in e2e test

--- mail/tests/test_end_to_end.py
+++ mail/tests/test_end_to_end.py
@@ -1,16 +1,34 @@
-from urllib.parse import quote
+import json
 
+import pytest
 import requests
 from django.conf import settings
-from django.test import override_settings
 from django.urls import reverse
+from freezegun import freeze_time
+from pytest_django.asserts import assertQuerySetEqual
+from rest_framework import status
 
 from mail.celery_tasks import send_licence_details_to_hmrc
-from mail.tests.libraries.client import LiteHMRCTestClient
+from mail.enums import ReceptionStatusEnum
+from mail.libraries.helpers import read_file
+from mail.models import LicenceData, LicencePayload, Mail
 
 
-def clear_stmp_mailbox():
+@pytest.fixture(autouse=True)
+def set_settings(settings):
+    settings.EMAIL_HOSTNAME = settings.TEST_EMAIL_HOSTNAME
+    settings.EMAIL_USER = "spire-to-dit-user"
+    settings.EMAIL_PASSWORD = "password"
+
+
+@pytest.fixture(autouse=True)
+def clear_stmp_mailbox(settings):
     requests.delete(f"http://{settings.TEST_EMAIL_HOSTNAME}:8025/api/v1/messages")
+
+
+@pytest.fixture()
+def licence_payload_json():
+    return json.loads(read_file("mail/tests/files/licence_payload_file", encoding="utf-8"))
 
 
 def get_smtp_body():
@@ -28,29 +46,43 @@ def get_smtp_body():
     return response.content.decode("ascii")
 
 
-@override_settings(
-    EMAIL_HOSTNAME=settings.TEST_EMAIL_HOSTNAME,
-    EMAIL_USER="spire-to-dit-user",
-    EMAIL_PASSWORD="password",
-)
-class EndToEndTests(LiteHMRCTestClient):
-    def test_send_email_to_hmrc_e2e(self):
-        clear_stmp_mailbox()
-        response = self.client.post(
-            reverse("mail:update_licence"),
-            data=self.licence_payload_json,
-            content_type="application/json",
-        )
-        self.assertEqual(
-            response.status_code,
-            200,
-        )
-        send_licence_details_to_hmrc.delay()
-        body = get_smtp_body()
-        body = body.replace("\r", "")
-        ymdhm_timestamp = body.split("\n")[0].split("\\")[5]
-        run_number = body.split("\n")[0].split("\\")[6]
-        expected_mail_body = rf"""1\fileHeader\SPIRE\CHIEF\licenceData\{ymdhm_timestamp}\{run_number}\N
+@pytest.mark.django_db()
+@freeze_time("2020-01-01")
+def test_send_email_to_hmrc_e2e(client, licence_payload_json):
+    assert not LicencePayload.objects.exists()
+
+    response = client.post(
+        reverse("mail:update_licence"),
+        data=licence_payload_json,
+        content_type="application/json",
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+    assert LicencePayload.objects.count() == 1
+    licence_payload = LicencePayload.objects.get()
+    assert not licence_payload.is_processed
+
+    assert not LicenceData.objects.exists()
+
+    assert not Mail.objects.exists()
+
+    send_licence_details_to_hmrc.delay()
+
+    licence_payload.refresh_from_db()
+    assert licence_payload.is_processed
+
+    assert LicenceData.objects.count() == 1
+    licence_data = LicenceData.objects.get()
+    assert licence_data.hmrc_run_number == 1
+    assertQuerySetEqual(licence_data.licence_payloads.all(), [licence_payload])
+
+    assert Mail.objects.count() == 1
+    mail = Mail.objects.get()
+    assert mail.status == ReceptionStatusEnum.REPLY_PENDING
+
+    assert licence_data.mail == mail
+
+    body = get_smtp_body().replace("\r", "")
+    expected_mail_body = rf"""1\fileHeader\SPIRE\CHIEF\licenceData\202001010000\1\N
 2\licence\20200000001P\insert\GBSIEL/2020/0000001/P\SIE\E\20200602\20220602
 3\trader\\GB123456789000\20200602\20220602\Organisation\might 248 James Key Apt. 515 Apt.\942 West Ashleyton Farnborough\Apt. 942\West Ashleyton\Farnborough\GU40 2LX
 4\country\GB\\D
@@ -67,13 +99,4 @@ class EndToEndTests(LiteHMRCTestClient):
 15\end\licence\14
 16\fileTrailer\1
 """
-        self.assertEqual(
-            body,
-            expected_mail_body,
-        )
-        encoded_reference_code = quote("GBSIEL/2020/0000001/P", safe="")
-        response = self.client.get(f"{reverse('mail:licence')}?id={encoded_reference_code}")
-        self.assertEqual(
-            response.json()["status"],
-            "reply_pending",
-        )
+    assert body == expected_mail_body
