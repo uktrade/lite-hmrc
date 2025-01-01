8a6094d0665fa3a71b4d9c4f64165ed8e97bb38c
Kevin Carrogan <kevin@carrogan.com>
2025-01-01 16:47:05 +0000

    Handle parsing of licence usage lines

--- edifact/tests/test_parsers.py
+++ edifact/tests/test_parsers.py
@@ -9,9 +9,10 @@ class UsageDataEdifactParserTests(TestCase):
         file = """1\\fileHeader\\CHIEF\\SPIRE\\usageData\\201901130300\\49543\\
 2\\licenceUsage\\LU04148/00001\\insert\\GBOIE2017/12345B\\O\\
 3\\line\\1\\0\\0\\GBP
-4\\end\\line\\3
-5\\end\\licenceUsage\\0
-6\\fileTrailer\\1"""
+4\\usage\\O\\9GB000001328000-PE112345\\R\\20190112\\0\\0\\\\000262
+5\\end\\line\\3
+6\\end\\licenceUsage\\5
+7\\fileTrailer\\1"""
 
         tree = usage_data_parser.parse(file)
         self.assertEqual(
@@ -45,10 +46,22 @@ class UsageDataEdifactParserTests(TestCase):
                                             Token("CURRENCY", "GBP"),
                                         ],
                                     ),
+                                    Tree(
+                                        Token("RULE", "licence_usage"),
+                                        [
+                                            Token("USAGE_TYPE", "O"),
+                                            Token("DECLARATION_UCR", "9GB000001328000-PE112345"),
+                                            Token("DECLARATION_PART_NUM", "R"),
+                                            Token("CONTROL_DATE", "20190112"),
+                                            Token("QUANTITY_USED", "0"),
+                                            Token("VALUE_USED", "0"),
+                                            Token("TRADER_ID", "000262"),
+                                        ],
+                                    ),
                                     Tree(Token("RULE", "licence_line_trailer"), [Token("RECORD_COUNT", "3")]),
                                 ],
                             ),
-                            Tree(Token("RULE", "licence_usage_transaction_trailer"), [Token("RECORD_COUNT", "0")]),
+                            Tree(Token("RULE", "licence_usage_transaction_trailer"), [Token("RECORD_COUNT", "5")]),
                         ],
                     ),
                     Tree(Token("RULE", "file_trailer"), [Token("LICENCE_USAGE_COUNT", "1")]),
