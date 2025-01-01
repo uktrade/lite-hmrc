b817ccc30e13b3b4a91eaa86e7933ae8e333df4d
Kevin Carrogan <kevin@carrogan.com>
2025-01-01 16:30:39 +0000

    Add parsing of licence lines

--- edifact/tests/test_parsers.py
+++ edifact/tests/test_parsers.py
@@ -8,11 +8,12 @@ class UsageDataEdifactParserTests(TestCase):
     def test_parsing_usage_data(self):
         file = """1\\fileHeader\\CHIEF\\SPIRE\\usageData\\201901130300\\49543\\
 2\\licenceUsage\\LU04148/00001\\insert\\GBOIE2017/12345B\\O\\
-3\\end\\licenceUsage\\0
-4\\fileTrailer\\1"""
+3\\line\\1\\0\\0\\GBP
+4\\end\\line\\3
+5\\end\\licenceUsage\\0
+6\\fileTrailer\\1"""
 
         tree = usage_data_parser.parse(file)
-
         self.assertEqual(
             tree,
             Tree(
@@ -30,6 +31,21 @@ class UsageDataEdifactParserTests(TestCase):
                                     Token("TRANSACTION_REF", "LU04148/00001"),
                                     Token("LICENCE_REF", "GBOIE2017/12345B"),
                                     Token("LICENCE_STATUS", "O"),
+                                ],
+                            ),
+                            Tree(
+                                Token("RULE", "licence_line"),
+                                [
+                                    Tree(
+                                        Token("RULE", "licence_line_header"),
+                                        [
+                                            Token("LINE_NUM", "1"),
+                                            Token("QUANTITY_USED", "0"),
+                                            Token("VALUE_USED", "0"),
+                                            Token("CURRENCY", "GBP"),
+                                        ],
+                                    ),
+                                    Tree(Token("RULE", "licence_line_trailer"), [Token("RECORD_COUNT", "3")]),
                                 ],
                             ),
                             Tree(Token("RULE", "licence_usage_transaction_trailer"), [Token("RECORD_COUNT", "0")]),
