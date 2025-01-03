d4765d4373fd7afc2e7ff577aa90ef2ef6eecc9d
Kevin Carrogan <kevin@carrogan.com>
2025-01-03 14:58:23 +0000

    Handle whitespace differently in usage data parser

--- edifact/parsers.py
+++ edifact/parsers.py
@@ -8,7 +8,7 @@ GRAMMARS_PATH = Path(settings.BASE_DIR) / "edifact" / "grammars"
 
 def generate_parser(grammar_file):
     full_file_path = GRAMMARS_PATH / grammar_file
-    return Lark.open(full_file_path, start="file")
+    return Lark.open(full_file_path, start="file", parser="lalr")
 
 
 usage_data_parser = generate_parser("usage_data.lark")
