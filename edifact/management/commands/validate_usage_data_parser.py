bc794154c71f01dc2c942a1285e61e5ee3b9c5ba
Kevin Carrogan <kevin@carrogan.com>
2025-01-06 15:23:36 +0000

    Check JSON output when parsing usage data

--- edifact/management/commands/validate_usage_data_parser.py
+++ edifact/management/commands/validate_usage_data_parser.py
@@ -4,7 +4,8 @@ from django.core.management import BaseCommand
 from lark.exceptions import LarkError
 
 from edifact.parsers import usage_data_parser
-from edifact.visitors import Edifact
+from edifact.visitors import Edifact, JsonPayload, SourceSplitter
+from mail.enums import SourceEnum
 from mail.models import UsageData
 
 logger = logging.getLogger(__name__)
@@ -36,6 +37,20 @@ class Command(BaseCommand):
                 self.stdout.write(
                     self.style.ERROR(f"Original edi data does not equal the transformed edi data for {usage_data}")
                 )
+                continue
+
+            try:
+                lite_only_data = SourceSplitter(SourceEnum.LITE).transform(tree)
+                transformed_json_data = JsonPayload().transform(lite_only_data)
+            except LarkError:
+                logger.warning("Failed to transform tree into JSON %s", usage_data, exc_info=True)
+                self.stdout.write(self.style.ERROR(f"Failed to transform tree for {usage_data}"))
+                continue
+
+            transformed_json_data["usage_data_id"] = str(usage_data.pk)
+            if transformed_json_data != usage_data.lite_payload:
+                logger.warning("Original JSON does not equal transformed JSON for %s", usage_data)
+                self.stdout.write(self.style.ERROR(f"Original JSON does not equal transformed JSON for {usage_data}"))
                 continue
 
             self.stdout.write(self.style.SUCCESS(f"Everything was parsed fine for {usage_data}"))
